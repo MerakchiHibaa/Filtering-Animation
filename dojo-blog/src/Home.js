6fba644c3b806e2e1d889f3673f8b46e547f5623
MerakchiHibaa <ninamimi305@gmail.com>
2022-02-22 20:40:44 +0100

    dependency arrays

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,32 +1,32 @@
-import {useState} from 'react' ;
+import {useState , useEffect} from 'react' ;
 import BlogList from './BlogList';
 const Home = () => {
   
-/* const [name , setName] = useState('Mario') ; 
-const  [age , setAge] = useState(20) ;
-const handleClick = () => {
-      setName('Luigi') ; 
-      setAge(30) ;
-    } */
+
    
     const [blogs , setBlogs] = useState(
         [ { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }]
     )
+    const [name ,setName] = useState('mario') ; 
     const handleDelete =(id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs) ;
     }
+    useEffect(()=> {
+       console.log('use effect ran') ; 
+       console.log(name) ;
+
+    } , [name])
 
     return ( 
         <div className="home"> 
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}></BlogList>
       <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's Blogs"></BlogList>
-
-{/*         <button onClick={handleClick}  > Click me</button>
- */}{/*         <button onClick ={()=>  handleClickAgain('mario')}> Click me again </button>
- */}        </div>
+<button onClick={()=> setName("Luigi")}> Change name </button>
+<p> {name}</p>
+        </div>
      );
 }
  
