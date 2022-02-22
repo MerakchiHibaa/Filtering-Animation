abe60f6d11172ddfdaedc1fcd03b0457f698c8ad
MerakchiHibaa <ninamimi305@gmail.com>
2022-02-22 20:04:17 +0100

    added the blogs

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,17 +1,30 @@
 import {useState} from 'react' ;
 const Home = () => {
   
-const [name , setName] = useState('Mario') ; 
+/* const [name , setName] = useState('Mario') ; 
+const  [age , setAge] = useState(20) ;
 const handleClick = () => {
       setName('Luigi') ; 
-    }
+      setAge(30) ;
+    } */
    
+    const [blogs , setBlogs] = useState(
+        [ { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }]
+    )
     return ( 
         <div className="home"> 
-        <h2>Homepage</h2>
-        <p> {name} </p>
-        <button onClick={handleClick}  > Click me</button>
-{/*         <button onClick ={()=>  handleClickAgain('mario')}> Click me again </button>
+      { blogs.map((blog)=>(
+          <div className="blog-preview" key={blog.id}> 
+          <h2> {blog.title} </h2>
+          <p>Written by {blog.author}</p>
+          
+          </div>
+
+      ))}
+{/*         <button onClick={handleClick}  > Click me</button>
+ */}{/*         <button onClick ={()=>  handleClickAgain('mario')}> Click me again </button>
  */}        </div>
      );
 }
