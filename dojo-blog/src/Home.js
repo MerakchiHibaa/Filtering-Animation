dc71578f7a8587baa6a806189b327915e979aacf
MerakchiHibaa <ninamimi305@gmail.com>
2022-02-22 23:32:28 +0100

    Loading

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,7 +4,8 @@ const Home = () => {
   
 
    
-    const [blogs , setBlogs] = useState(null)
+    const [blogs , setBlogs] = useState(null) ; 
+    const [isPending , setIsPending] = useState(true) ;
    /*  [ { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }]
@@ -15,17 +16,30 @@ const Home = () => {
         setBlogs(newBlogs) ;
     } */
     useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+     .then(res => {
+        return res.json() ;
+     })
+     .then(data => {
+setBlogs(data) ;
+setIsPending(false) ;
+ 
+     })
+    /*  setTimeout( ()=> { 
      fetch('http://localhost:8000/blogs')
      .then(res => {
         return res.json() ;
      })
      .then(data => {
 setBlogs(data) ;
-     })
+setIsPending(false) ;
+ 
+     }) } , 1000 ) ;  */
      } , [])
 
     return ( 
         <div className="home"> 
+        {isPending && <div> Loading...</div>}
      { blogs &&  <BlogList blogs={blogs} title="All Blogs" /* handleDelete={handleDelete} */></BlogList> }
      { blogs &&  <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's Blogs"></BlogList> }
         </div>
