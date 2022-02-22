4e3421d32bb65b7edde86175d787e44e877776bd
MerakchiHibaa <ninamimi305@gmail.com>
2022-02-22 20:27:10 +0100

    added the delete button and function

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -14,9 +14,14 @@ const handleClick = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }]
     )
+    const handleDelete =(id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs) ;
+    }
+
     return ( 
         <div className="home"> 
-      <BlogList blogs={blogs} title="All Blogs"></BlogList>
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}></BlogList>
       <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's Blogs"></BlogList>
 
 {/*         <button onClick={handleClick}  > Click me</button>
