6b4941e13a509697a868e7b0e37903a84d52796f
MerakchiHibaa <ninamimi305@gmail.com>
2022-02-22 19:28:54 +0100

    added the hook functions

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,18 @@
+import {useState} from 'react' ;
 const Home = () => {
+  
+const [name , setName] = useState('Mario') ; 
+const handleClick = () => {
+      setName('Luigi') ; 
+    }
+   
     return ( 
         <div className="home"> 
-        <h2>Homepage</h2></div>
+        <h2>Homepage</h2>
+        <p> {name} </p>
+        <button onClick={handleClick}  > Click me</button>
+{/*         <button onClick ={()=>  handleClickAgain('mario')}> Click me again </button>
+ */}        </div>
      );
 }
  
