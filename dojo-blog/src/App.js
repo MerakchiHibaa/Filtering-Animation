fe66f0cb4bb7bd583df229ad4860eb0f58fd7dea
MerakchiHibaa <ninamimi305@gmail.com>
2022-02-22 18:22:24 +0100

    created the navbar

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Navbar' ; 
+import Home from './Home' ;   
+
+function App() {
+  const title = "Welcome to the new blog" ;
+  return (
+    <div className="App">
+      <Navbar> </Navbar>
+      <div className="content">
+       <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
