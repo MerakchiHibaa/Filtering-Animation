242076028637039204179a035060b388712eabb6
MerakchiHibaa <ninamimi305@gmail.com>
2022-02-24 19:18:36 +0100

    react Router

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,15 +1,21 @@
 import Navbar from './Navbar' ; 
 import Home from './Home' ;   
-
+import {BrowserRouter as Router , Route ,Switch } from 'react-router-dom' ;
 function App() {
   const title = "Welcome to the new blog" ;
   return (
+    <Router>
     <div className="App">
-      <Navbar> </Navbar>
+      <Navbar /> 
       <div className="content">
-       <Home />
+      <Switch> 
+        <Route path="/"> 
+        <Home />
+        </Route>
+      </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
