011a3a30eebc59a4933a38f8df7db2b086c29585
MerakchiHibaa <ninamimi305@gmail.com>
2022-02-25 18:59:02 +0100

    deleting blogs

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,9 +1,18 @@
 import {useParams} from 'react-router-dom' ; 
+import {useHistory} from 'react-router-dom' ;
 import useFetch from './useFetch' ;
  const BlogDetails = () => {
     const {id} = useParams() ;
+    const history = useHistory() ;
     const { data : blog , error , isPending} = useFetch('http://localhost:8000/blogs/' + id) ; 
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/'+ blog.id , {
+            method : 'DELETE'
+        }).then(() => {
+            history.push('/') ;
+        })
 
+    }
     return ( 
         <div className="blog-details">
 {isPending && <div> Loading...</div> }
@@ -13,7 +22,10 @@ import useFetch from './useFetch' ;
         <h2> {blog.title}</h2>
         <p>Written by {blog.author}</p>
         <div> {blog.body}</div>
+        <button onClick={handleClick}> Delete</button>
+
     </article>
+
 )
 }
         </div>
