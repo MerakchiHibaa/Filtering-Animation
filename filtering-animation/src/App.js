3985b852b6ae96f427480c1dc6ae21950ea0ec4d
MerakchiHibaa <ninamimi305@gmail.com>
2022-02-25 21:49:24 +0100

    added the filters in the console

--- filtering-animation/src/App.js
+++ filtering-animation/src/App.js
@@ -4,6 +4,9 @@ import Filter from './Filter' ;
 import { useEffect , useState} from "react" ; 
 function App() {
   const [popular , setPopular] = useState([]) ; //the data we get is an array of
+  const [filtered , setFiltered] = useState([]) ; 
+  const [activeGenre , setActiveGenre] = useState(0) ; //the genre is set as a number in the coming requestData
+
   useEffect( ()=> {
     fetchPopular() ; 
   } , []) ; 
@@ -11,10 +14,15 @@ function App() {
     const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=2a4795bbacb34af632906cda4aa913a7&language=en-US&page=1') ; 
     const movies = await data.json();
     setPopular(movies.results) ; 
+    setFiltered(movies.results) ; 
   }
   return (
     <div className="App">
-      <Filter></Filter>
+      <Filter 
+      popular={popular} 
+      setFiltered={setFiltered} 
+      activeGenre={activeGenre} 
+      setActiveGenre={setActiveGenre}  />
      <div className="popular-movies">
        {popular.map(movie => {
          return <Movie key={movie.id} movie={movie} /> 
