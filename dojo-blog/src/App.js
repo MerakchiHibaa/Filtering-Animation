f6bc251570e3245dfab76ea93aaff281040bd867
MerakchiHibaa <ninamimi305@gmail.com>
2022-02-25 18:02:25 +0100

    Router parameters

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar' ; 
 import Home from './Home' ;   
 import Create from './Create' ; 
+import BlogDetails from './BlogDetails';
 import {BrowserRouter as Router , Route ,Switch } from 'react-router-dom' ;
 function App() {
   const title = "Welcome to the new blog" ;
@@ -10,12 +11,16 @@ function App() {
       <Navbar /> 
       <div className="content">
       <Switch> 
+      <Route exact path="/"> 
+        <Home />
+        </Route>
         <Route path="/create"> 
         <Create />
         </Route>
 
-        <Route exact path="/"> 
-        <Home />
+        
+        <Route path="/blogs/:id"> 
+        <BlogDetails />
         </Route>
       </Switch>
       </div>
--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,4 +1,6 @@
+import {Link} from "react-router-dom" ; 
 const BlogList = ({blogs , title  /* handleDelete */}) => {
+ 
     /* const blogs = props.blogs;
     const title = props.title */
    
@@ -8,8 +10,11 @@ const BlogList = ({blogs , title  /* handleDelete */}) => {
             <h2> {title}</h2>
 { blogs.map((blog)=>(
           <div className="blog-preview" key={blog.id}> 
+          <Link to={`/blogs/${blog.id}`}>
           <h2> {blog.title} </h2>
           <p>Written by {blog.author}</p>
+           </Link> 
+         
 {/*           <button onClick ={()=> handleDelete(blog.id)}> Delete blog</button>
  */}          </div>
 
